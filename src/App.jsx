2965cb5c64fa54a6ee76971adace5280e7a6a1e3
Munachimso Aneke <52995219+anekemuna@users.noreply.github.com>
2025-10-28 21:17:24 -0700

    Add basic search functionality

--- src/App.jsx
+++ src/App.jsx
@@ -7,10 +7,13 @@ import List from "./components/List";
 
 function App() {
   const [list, setList] = useState([]);
+  const [filteredResult, setFilteredResult] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [searchInput, setSearchInput] = useState("");
+
   useEffect(() => {
     const fetchPokemon = async () => {
-      const API = "https://pokeapi.co/api/v2/pokemon?limit=10";
+      const API = "https://pokeapi.co/api/v2/pokemon?limit=20";
 
       const response = await fetch(API);
       const json = await response.json();
@@ -28,12 +31,36 @@ function App() {
     fetchPokemon().catch(console.error);
   }, []);
 
+  const searchItems = (searchValue) => {
+    setSearchInput(searchValue);
+    if (searchValue !== "") {
+      const filteredPokemon = list.filter(
+        (pokemon) =>
+          pokemon.name.toLowerCase().includes(searchValue.toLowerCase()) ||
+          pokemon.types.some((type) =>
+            type.type.name.toLowerCase().includes(searchValue.toLowerCase())
+          )
+      );
+      setFilteredResult(filteredPokemon);
+    } else {
+      setFilteredResult(list);
+    }
+  };
+
   return (
     <div className="app">
       <NavBar />
       <div className="app-body">
-        <Header loading={loading} data={list} />
-        <List loading={loading} data={list} />
+        <Header loading={loading} data={searchInput.length > 0 ? filteredResult : list} />
+        <div>
+          <span>🔎</span>
+          <input
+            type="text"
+            placeholder="Search..."
+            onChange={(inputString) => searchItems(inputString.target.value)}
+          />
+        </div>
+        <List loading={loading} data={searchInput.length > 0 ? filteredResult : list} />
       </div>
     </div>
   );
