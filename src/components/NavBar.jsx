ba5f9c955b8418150081de2686c820160647f54c
Munachimso Aneke <52995219+anekemuna@users.noreply.github.com>
2025-10-28 18:22:28 -0700

    Fix Navbar styling

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,14 +2,14 @@ import React from "react";
 
 const NavBar = () => {
   return (
-    <>
-      <div className="navbar">NavBar</div>
+    <div className="navbar">
+      NavBar
       <ul className="nav-list">
         <li>Dashboard</li>
         <li>Search</li>
         <li>About</li>
       </ul>
-    </>
+    </div>
   );
 };
 
